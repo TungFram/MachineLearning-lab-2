097ca05fe139d0c2e46c5d28354c7a43056a948d
Vladimir Sidortsov <tungfram@yandex.ru>
2022-10-09 02:12:32 +0300

    fix: tasks 1-3 without cycles on tables

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ filenames.clear()
 share_urls.clear()
 ids.clear()
 download_links.clear()
+
+
 # files.clear()
 
 def print_dataframe(dataframe):
@@ -83,49 +85,41 @@ gender_train = pd.read_csv(filepath_or_buffer=files_info['file_name']['gender_tr
                            index_col='customer_id',
                            usecols=['customer_id', 'gender'])
 
-
 # ############################################### Task 1 #########################################################
 # Для столбца tr_type датафрейма transactions выберите произвольные 1000 строк с помощью метода sample
 # В полученной на предыдущем этапе подвыборке найдите долю транзакций 
 # (стобец tr_description в датасете tr_types), в которой содержится подстрока 'POS' или 'ATM'.
 
-# В цикле по типам найти этот тип в другой таблице и проверить его описание. Если описание содержит нужный паттерн - 
-# увеличить количество. 
-def count_strs_contained_pattern(sample, sub_sample, pattern):
-    count = 0
-    for item in sub_sample:
-        if re.search(pattern, sample.loc[item]) is not None:
-            count += 1
-    return count
-
-
 subsample = transactions['tr_type'].sample(1000)  # Series of types
+subsample_info = pd.DataFrame(
+    index=subsample,
+    columns=['tr_description']
+)
+subsample_info.update(tr_types.tr_description)
 
-# Иногда выдает ошибку, но работает!
-# types_contained_condition = count_strs_contained_pattern(sample=tr_types.tr_description,
-#                                                          sub_sample=subsample,
-#                                                          pattern='POS|АТМ')
+pattern = 'POS|АТМ'
+tr_types_contained_pattern = subsample_info[(subsample_info.tr_description.str.contains(pattern)) == True]
 
-# print("Соотношение имеющих в описании типа транзакции POS|АТМ ко всем типам транзакции в случайной выборке = ",
-#       types_contained_condition / subsample.size)
+print("\nСоотношение имеющих в описании типа транзакции POS|АТМ ко всем типам транзакции в случайной выборке = ",
+      tr_types_contained_pattern.size / subsample_info.size)
 
 # ############################################### Task 2 #########################################################
 # Для столбца tr_type датафрейма transactions посчитайте частоту встречаемости всех типов
 # транзакций tr_type в transactions. 
 # Выведите топ-10 транзакций по частоте встречаемости (вывести для них tr_description тоже). 
 
+
 types_frequency = pd.DataFrame(
     index=tr_types.index,
     columns=['occurrence', 'tr_description'],
 )
 
-for type_of_transaction in tr_types.index:
-    types_frequency.occurrence[type_of_transaction] = \
-        transactions.tr_type[(transactions.tr_type == type_of_transaction)].size
-    types_frequency.tr_description[type_of_transaction] = tr_types.tr_description[type_of_transaction]
+types_frequency.occurrence = transactions.tr_type.value_counts()
+types_frequency.update(tr_types.tr_description)
+top10 = types_frequency.sort_values(by='occurrence', ignore_index=False, ascending=False).head(10)
 
-sorted_occurrence = types_frequency.sort_values(by='occurrence', ignore_index=False, ascending=False).head(10)
-print_dataframe(sorted_occurrence)
+print("\nТоп 10 встерчаемых типов транзакций, их частота и описание: ")
+print_dataframe(top10)
 
 # ############################################### Task 3 #########################################################
 # В датафрейме transactions найдите клиента с максимальной суммой приходов на карту.
@@ -140,14 +134,8 @@ customer_info = pd.DataFrame(
 groups_of_customer = transactions.groupby(level='customer_id')
 
 for customer_id, operations in groups_of_customer:
-    positive_sum = 0
-    negative_sum = 0
-
-    for number in operations.amount:
-        if number > 0:
-            positive_sum += number
-        else:
-            negative_sum += number
+    positive_sum = operations.amount[operations.amount > 0.0].sum()
+    negative_sum = operations.amount[operations.amount < 0.0].sum()
 
     customer = pd.Series(data={
         'expenses': round(negative_sum, 3),
@@ -155,7 +143,7 @@ for customer_id, operations in groups_of_customer:
     })
     customer_info.loc[customer_id] = customer
 
-customer_info['difference'] = np.abs(customer_info.deposits - customer_info.expenses)
+customer_info['difference'] = np.abs(customer_info.deposits + customer_info.expenses)
 
 min_customer = customer_info.sort_values(by='expenses', ignore_index=False, ascending=True).head(1)
 max_customer = customer_info.sort_values(by='deposits', ignore_index=False, ascending=False).head(1)
@@ -166,45 +154,57 @@ print_dataframe(max_customer)
 print("\nКлиент с максимальной суммой расходов на карту: ")
 print_dataframe(min_customer)
 
-# ############################################### Task 4 #########################################################
-# Найдите среднее арифметическое и медиану по amount по всем типам транзакций из топ 10 из задания 2
-# Найдите среднее арифметическое и медиану по amount по всем типам транзакций для клиентов из задания 3
+# # ############################################### Task 4 #########################################################
+# # Найдите среднее арифметическое и медиану по amount по всем типам транзакций из топ 10 из задания 2
+# # Найдите среднее арифметическое и медиану по amount по всем типам транзакций для клиентов из задания 3
 
 tr_types_top10_info = pd.DataFrame(
-    columns=sorted_occurrence.index.unique()
+    index=top10.index,
+    columns=['mean', 'median']
 )
 
-for type_of_transaction in sorted_occurrence.index.unique():
-    tr_types_top10_info[type_of_transaction] = transactions[transactions.tr_type == type_of_transaction].amount.describe()
+all_top10_trs = transactions[transactions.tr_type.isin(top10.index)]
+all_top10_tr_groups = all_top10_trs.groupby('tr_type')
+
+tr_types_top10_info['mean'] = all_top10_tr_groups.amount.mean()
+tr_types_top10_info['median'] = all_top10_tr_groups.amount.median()
 
 print("\nИнформация о amount по всем типам транзакций из топ 10 (результат задания 2):")
 print_dataframe(tr_types_top10_info)
 
-tr_types_min_customer_info = pd.DataFrame()
-tr_types_max_customer_info = pd.DataFrame()
-min_customer_id = min_customer.index[0]
-max_customer_id = max_customer.index[0]
+# надо написать функцию, которая дает все типы транзакций с их описанием у клиента. 
 
-for type_of_transaction in transactions.loc[min_customer_id].tr_type.unique():
-    this_cus_operations = transactions.loc[min_customer_id]
-    tr_types_min_customer_info[type_of_transaction] = \
-        this_cus_operations[this_cus_operations.tr_type == type_of_transaction].amount.describe()
+# tr_types_min_customer_info = pd.DataFrame()
+# tr_types_max_customer_info = pd.DataFrame()
+# min_customer_id = min_customer.index[0]
+# max_customer_id = max_customer.index[0]
+# 
+# for type_of_transaction in transactions.loc[min_customer_id].tr_type.unique():
+#     this_cus_operations = transactions.loc[min_customer_id]
+#     tr_types_min_customer_info[type_of_transaction] = \
+#         this_cus_operations[this_cus_operations.tr_type == type_of_transaction].amount.describe()
 
-if min_customer_id == max_customer_id:
-    print("\nТ.к. идентификаторы клиента с максимальной суммой приходов на карту и "
-          "клиента с максимальной суммой расходов по карте совпадают, таблица для одного человека только одна:")
-    print_dataframe(tr_types_min_customer_info)
-else:
-    print("\nИнформация о amount по всем типам транзакций для клиента с максимальной суммой расходов по карте"
-          " (результат задания 3):")
-    print_dataframe(tr_types_min_customer_info)
-    
-    for type_of_transaction in transactions.loc[max_customer_id].tr_type.unique():
-        this_cus_operations = transactions.loc[max_customer_id]
-        tr_types_max_customer_info[type_of_transaction] = \
-            this_cus_operations[this_cus_operations.tr_type == type_of_transaction].amount.describe()
-    
-    print("\nИнформация о amount по всем типам транзакций для клиента с максимальной суммой приходов на карту"
-          " (результат задания 3):")
-    print_dataframe(tr_types_max_customer_info)
-
+# if min_customer_id == max_customer_id:
+#     print("\nТ.к. идентификаторы клиента с максимальной суммой приходов на карту и "
+#           "клиента с максимальной суммой расходов по карте совпадают, таблица для одного человека только одна:")
+#     print_dataframe(tr_types_min_customer_info)
+# else:
+#     print("\nИнформация о amount по всем типам транзакций для клиента с максимальной суммой расходов по карте"
+#           " (результат задания 3):")
+#     print_dataframe(tr_types_min_customer_info)
+# 
+#     for type_of_transaction in transactions.loc[max_customer_id].tr_type.unique():
+#         this_cus_operations = transactions.loc[max_customer_id]
+#         tr_types_max_customer_info[type_of_transaction] = \
+#             this_cus_operations[this_cus_operations.tr_type == type_of_transaction].amount.describe()
+# 
+#     print("\nИнформация о amount по всем типам транзакций для клиента с максимальной суммой приходов на карту"
+#           " (результат задания 3):")
+#     print_dataframe(tr_types_max_customer_info)
+# 
+# # ############################################### Task 5 #########################################################
+# 
+# transactions = pd.merge(transactions, gender_train, how='left')
+# transactions = pd.merge(transactions, tr_mcc_codes, how='inner')
+# transactions = pd.merge(transactions, tr_types, how='inner')
+# transactions.shape
